cafe93c196cf64f11fdcfeec7010bc793ab2ce19
analuizalamas <analuiza.lamas@mercadolivre.com>
2022-07-25 19:42:01 -0300

    Exercicios assinncronos e de aulas ao vivo

--- routes/routeUser.js
+++ routes/routeUser.js
@@ -1,4 +1,4 @@
-const express = require('express');
+const express = require("express");
 
 const routes = express.Router();
 
@@ -25,60 +25,77 @@ console.log('foo.bargmail.com =>', emailRegex.test('foo.bar
 
 */
 
-// POST
-routes.post('/', (req, res) => {
-    const newUser = req.body;
+// POST para adicionar um usuário seguindo as propriedades citadas.
+routes.post("/", (req, res) => {
+  const newUser = req.body;
 
-    users = [...users, newUser];
-    
-    return res.status(201).json({message: "Usuario criado com sucesso!"})
+  users = [...users, newUser];
+
+  return res.status(201).json({ message: "Usuário criado com sucesso!" });
 });
 
-// PUT  
-    routes.put('/:id', (req, res) => {
-        const id = Number(req.params.id);
-        const content = req.body;
-    
-        const findUser = users.findIndex((user) => user.id === id);
-        products[findUser] = content;
-    
-        res.status(200).json({message: "Produto atualizado com sucesso!"});
-    })
+// PUT para modificar informações de um usuário.
+routes.put("/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const content = req.body;
 
-    // GET 
-routes.get('/', (req,res) =>{
-    res.status(200).json(users);
-})
+  const findUser = users.findIndex((user) => user.id === id);
+  products[findUser] = content;
 
-routes.get("/:id", (req, res) =>{
-    const id = Number(req.params.id);
-
-    const details = req.params.details || 'Sem descrição.';
-    console.log(details);
-
-    const email = req.params.email;
-    console.log(email)
-
-    const user = users.find((user) => user.id === id)
-
-    res.status(200).json(user);
-    
+  res.status(200).json({ message: "Produto atualizado com sucesso!" });
 });
 
-// DELETE 
+// GET para verificar a lista de usuários.
+routes.get("/", (req, res) => {
+  res.status(200).json(users);
+});
+
+routes.get("/:id", (req, res) => {
+  const id = Number(req.params.id);
+
+  const details = req.params.details || "Sem descrição.";
+  console.log(details);
+
+  const email = req.params.email;
+  console.log(email);
+
+  const user = users.find((user) => user.id === id);
+
+  res.status(200).json(user);
+});
+
+// DELETE para deletar um usuário.
 
 routes.delete("/:id", (req, res) => {
-    const id = Number(req.params.id);
-  
-    const username = users.find((user) => user.id === id);
-  
-     if (!username) {
-      return res.status(400).json({"message":"Usuário não encontrado"})
-     }
-     users = users.filter((user) => user.id !== id);
-      
-     res.status(200).json(users);
+  const id = Number(req.params.id);
+
+  const username = users.find((user) => user.id === id);
+
+  if (!username) {
+    return res.status(400).json({ message: "Usuário não encontrado" });
+  }
+  users = users.filter((user) => user.id !== id);
+
+  res.status(200).json(users);
+});
+
+// Pesquise sobre o método PATCH e faça um endpoint que permita atualizar APENAS a senha do usuário.
+routes.patch("/:id", (req, res) => {
+  const { id } = req.params;
+  const change = req.body;
+
+  if (Object.keys(change) != "password") {
+    return res.send(403).json();
+  }
+
+  users.forEach((user) => {
+    if (user.id == id) {
+      user.password = change.password;
+    }
   });
 
-  //exportado
-  module.exports = routes;
+  return res.status(200).json(users);
+});
+
+//exportado
+module.exports = routes;
